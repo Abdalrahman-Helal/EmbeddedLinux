d02974749ce8fc16a751a8348ddeea0c8238d193
Abdalrahman Helal <Abdlrhmnhelal@gmail.com>
2025-11-15 22:37:03 +0200

    Updated Notes and Added IfCondition.sh

---
+++ BashScripting/Scripts/IfCondition.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+
+#Test String
+# test "Helal" = "Helal" && echo true
+# test "Helal" = "Ahmed" && echo true || echo False
+
+
+#Test Integer
+# test 001 = 1 && echo Integer || echo String  # it consisders it as string not integer 
+# test 001 -eq 1 && echo Integer || echo String
+
+
+#if condition
+
+# if [ 2 -eq 1 ]
+# then 
+# echo "the value is 1"
+# else
+# echo "the value is not 1"
+# fi
+
+
+# read -p "Enter String " str
+# # -z means string is Zero
+# if [ -z $str ]
+# then
+# echo "Empty"
+# else
+# echo "the string is ${str}"
+# fi
+
+
+# read -p "Enter Number : " number
+# if [ $number -lt 125 ]
+# then 
+#     echo "Value is less than 125"
+# elif [ $number -lt 200 ] 
+# then
+#     echo "the value is between 125 and 200"
+# else 
+#     echo "valuie is greater tha 200"
+# fi 
+
+
+
+# read -p "variable x " x
+# test -z $x && echo "Empty"
+
+
+
+test -f "./tessst.sh" && echo "File Exist" || echo "File Doesn't Exist"
+
+if [ -f "./test.sh" ]
+then
+    echo "File Exist"
+else
+    echo "File Doesn't Exist"
+fi
